1fc58424250eb017ea3272dcd9eba20430a62857
illonis <things@illonis.de>
2012-10-11 22:29:01 +0200

    initial commit

---
+++ Eduras/src/de/illonis/eduras/Eduras.java
@@ -0,0 +1,15 @@
+package de.illonis.eduras;
+
+public class Eduras {
+
+	public Eduras() {
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello git!");
+	}
+}
---
+++ Eduras/src/de/illonis/eduras/exceptions/UserTooStupidException.java
@@ -0,0 +1,10 @@
+package de.illonis.eduras.exceptions;
+
+public class UserTooStupidException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public UserTooStupidException() {
+
+	}
+}
