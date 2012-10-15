c1b85b340241872c5510595036fec33cdeac530f
Florian Mai <florian.ren.mai@googlemail.com>
2012-10-15 23:44:15 +0200

    [STRUCTURE] Added an exception for special purpose.

---
+++ Eduras/src/de/illonis/eduras/exceptions/GivenParametersDoNotFitToEventException.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package de.illonis.eduras.exceptions;
+
+/**
+ * Exception thrown when a parameter, especially a GameEventNumber, does
+ * not fit to the event the parameter was associated with.
+ * @author Florian Mai <florian.ren.mai@googlemail.com>
+ *
+ */
+public class GivenParametersDoNotFitToEventException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
