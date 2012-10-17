ce345c9321b3e9110c33a9d32b395947adf4ba1a
illonis <things@illonis.de>
2012-10-17 22:06:56 +0200

    [NETWORK][BUFFER] Elaborated buffers so they only provide methods they allow to do.

--- Eduras/src/de/illonis/eduras/networking/Buffer.java
+++ Eduras/src/de/illonis/eduras/networking/Buffer.java
@@ -1,55 +1,65 @@
 package de.illonis.eduras.networking;
 
 import java.util.LinkedList;
-import java.util.NoSuchElementException;
 
 import de.illonis.eduras.exceptions.BufferIsEmptyException;
 
 /**
- * A Buffer is a linked list of Strings. Oldest elements will be popped first.
+ * A Buffer is a linked list of Strings. Elements are returned in same order
+ * they where added.
  * 
  * @author illonis
  * 
  */
-public class Buffer extends LinkedList<String> {
+public class Buffer {
+
 	/**
 	 * Use this object to synchronize access.
 	 */
 	public final static Object SYNCER = new Object();
 
-	private static final long serialVersionUID = 1L;
+	private LinkedList<String> list;
 
 	/**
-	 * Returns the first element of buffer. The first element is that element
-	 * that was added before all other elements.
+	 * Creates a new buffer.
+	 */
+	public Buffer() {
+		list = new LinkedList<String>();
+	}
+
+	/**
+	 * Returns and removes the first element of buffer. The first element is
+	 * that element that was added before all other elements.<br>
+	 * Elements are returned in same order they where added.
 	 * 
 	 * @return First element of buffer.
-	 * @throws NoSuchElementException
-	 *             Thrown when list is empty.
+	 * @throws BufferIsEmptyException
+	 *             Thrown if list is empty.
 	 */
 	public String getNext() throws BufferIsEmptyException {
-		return pop();
+		return list.pop();
 	}
 
 	/**
-	 * Appends a string at the and of the buffer.
+	 * Appends a string at the end of this buffer.
 	 * 
 	 * @param string
-	 *            string to add.
+	 *            String to add.
 	 */
 	public void append(String string) {
-		add(string);
+		list.add(string);
 	}
 
 	/**
-	 * Returns all strings that are in this buffer.
+	 * Returns all strings that are in this buffer as an array.
 	 * 
 	 * @return A string array containing all strings.
+	 * @throws BufferIsEmptyException
+	 *             Thrown if list is empty.
 	 */
 	public String[] getAll() throws BufferIsEmptyException {
-		if (size() == 0)
+		if (list.size() == 0)
 			throw new BufferIsEmptyException();
-		return (String[]) toArray();
+		return (String[]) list.toArray();
 	}
-
 }
