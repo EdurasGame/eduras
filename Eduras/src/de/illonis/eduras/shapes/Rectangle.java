3c99b01a75f2b26a376e5e3d42a1d04ea3fb10f7
Florian Mai <florian.ren.mai@googlemail.com>
2012-12-01 16:50:42 +0100

    [LOGIC][SHAPE] Introduced rectangles.

---
+++ Eduras/src/de/illonis/eduras/shapes/Rectangle.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package de.illonis.eduras.shapes;
+
+import de.illonis.eduras.exceptions.ShapeVerticesNotApplicableException;
+import de.illonis.eduras.math.Vector2D;
+
+/**
+ * Represents a rectangle.
+ * 
+ * @author Florian Mai <florian.ren.mai@googlemail.com>
+ * 
+ */
+public class Rectangle extends Polygon {
+
+	/**
+	 * Creates a rectangle made of the given point at the top left and the point
+	 * at bottom right.
+	 * 
+	 * @param topLeft
+	 *            The point at top left.
+	 * @param bottomRight
+	 *            The point at bottom right.
+	 * @throws ShapeVerticesNotApplicableException
+	 *             Thrown if the topleft point is not the topleft point or the
+	 *             bottom right point is not the bottom right point or if the
+	 *             origin (0,0) does not lie inside of the rectangle.
+	 */
+	public Rectangle(Vector2D topLeft, Vector2D bottomRight)
+			throws ShapeVerticesNotApplicableException {
+		if (topLeft.getX() > 0 || topLeft.getY() < 0 || bottomRight.getX() < 0
+				|| bottomRight.getY() > 0) {
+			throw new ShapeVerticesNotApplicableException();
+		} else {
+			Vector2D[] vertices = new Vector2D[4];
+			vertices[0] = topLeft;
+			vertices[1] = new Vector2D(bottomRight.getX(), topLeft.getY());
+			vertices[2] = new Vector2D(topLeft.getX(), bottomRight.getY());
+			vertices[3] = bottomRight;
+			setVertices(vertices);
+		}
+	}
+
+}
