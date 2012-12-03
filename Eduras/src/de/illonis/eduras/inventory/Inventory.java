ac552681b3b1e33ca23bd4b2109fedad21ea4a8d
illonis <things@illonis.de>
2012-12-03 20:44:12 +0100

    [ITEMS][INVENTORY] Implemented basic inventory functions.

--- Eduras/src/de/illonis/eduras/inventory/Inventory.java
+++ Eduras/src/de/illonis/eduras/inventory/Inventory.java
@@ -1,6 +1,7 @@
 package de.illonis.eduras.inventory;
 
 import de.illonis.eduras.items.Item;
+import de.illonis.eduras.items.Item.ItemType;
 import de.illonis.eduras.items.StackableItem;
 
 public class Inventory {
@@ -18,10 +19,21 @@ public class Inventory {
 		}
 	}
 
+	/**
+	 * Returns gold amount hold in inventory.
+	 * 
+	 * @return current gold amount.
+	 */
 	public int getGold() {
 		return gold;
 	}
 
+	/**
+	 * Returns number of items currently hold in inventory. Item stacks count as
+	 * a single item.
+	 * 
+	 * @return number of items in inventory.
+	 */
 	public int getNumItems() {
 		int n = 0;
 		for (int i = 0; i < MAX_CAPACITY; i++) {
@@ -31,17 +43,92 @@ public class Inventory {
 		return n;
 	}
 
+	/**
+	 * Adds given amount of money to inventory.
+	 * 
+	 * @param amount
+	 *            amount to add.
+	 */
 	public synchronized void addGold(int amount) {
 		gold += amount;
 	}
 
+	/**
+	 * Buys an item and puts it into next free inventory slot.
+	 * 
+	 * @param item
+	 *            item to add.
+	 */
 	public void buy(Item item) {
 
 	}
 
 	/**
-	 * Sells item in given inventory slot and adds gained money to player gold.
-	 * If selling item is stackable, only one item of stack is selled.
+	 * Returns first occurence of item of given {@link ItemType}. If there is no
+	 * item of given type, -1 is returned.
+	 * 
+	 * @param type
+	 *            item type to search for.
+	 * @return index of item with given type or -1.
+	 */
+	private int getItemOfType(ItemType type) {
+		return getItemOfTypeBetween(type, 0, MAX_CAPACITY);
+	}
+
+	private int getItemOfTypeBetween(ItemType type, int from, int to) {
+		for (int i = from; i < to; i++) {
+			try {
+				if (itemSlots[i].getItem().getType().equals(type))
+					return i;
+			} catch (ItemSlotIsEmptyException e) {
+			}
+		}
+		return -1;
+	}
+
+	private synchronized int findNextFreeInventorySlotForItem(Item item) {
+
+		int targetPos = -1;
+
+		if (item.stacks()) {
+			int left = 0;
+			do {
+				targetPos = getItemOfTypeBetween(item.getType(), left,
+						MAX_CAPACITY);
+				if (targetPos >= 0) {
+					try {
+						if (((StackableItem) itemSlots[targetPos].getItem())
+								.isFull()) {
+							left++;
+						} else {
+							break;
+						}
+					} catch (ItemSlotIsEmptyException e) {
+						e.printStackTrace();
+					}
+				}
+
+			} while (left < MAX_CAPACITY);
+		} else {
+			targetPos = findNextFreeInventorySlot();
+			if (targetPos == -1)
+				return -1;
+		}
+
+		return -1;
+	}
+
+	private int findNextFreeInventorySlot() {
+		for (int i = 0; i < MAX_CAPACITY; i++) {
+			if (!itemSlots[i].hasItem())
+				return i;
+		}
+		return -1;
+	}
+
+	/**
+	 * Sells the item in given inventory slot and adds gained money to player
+	 * gold. If selling item is stackable, only one item of stack is sold.
 	 * 
 	 * @param itemSlot
 	 *            itemslot to sell.
@@ -51,7 +138,7 @@ public class Inventory {
 	public synchronized void sell(int itemSlot) throws ItemSlotIsEmptyException {
 		Item item = itemSlots[itemSlot].getItem();
 		int price = item.getSellValue();
-		gold += price;
+		addGold(price);
 		if (item.stacks()) {
 			StackableItem si = (StackableItem) item;
 			si.takeFromStack();
