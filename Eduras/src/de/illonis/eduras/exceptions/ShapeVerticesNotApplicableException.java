b62b30b837a119ec7e0b6c27fc0ddf3c837f2473
illonis <things@illonis.de>
2013-01-20 19:23:30 +0100

    [OBJECTS][SHAPES] Added JavaDoc to ShapeVerticesNotApplicableException.

--- Eduras/src/de/illonis/eduras/exceptions/ShapeVerticesNotApplicableException.java
+++ Eduras/src/de/illonis/eduras/exceptions/ShapeVerticesNotApplicableException.java
@@ -4,14 +4,13 @@
 package de.illonis.eduras.exceptions;
 
 /**
+ * Thrown when an invalid shape was tried to create.
+ * 
  * @author Florian Mai <florian.ren.mai@googlemail.com>
  * 
  */
 public class ShapeVerticesNotApplicableException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 }
