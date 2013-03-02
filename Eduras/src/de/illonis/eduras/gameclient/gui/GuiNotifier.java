36008a88aabd08182c2b825988f59c9488199fe8
illonis <things@illonis.de>
2013-03-02 16:28:43 +0100

    [CLIENT][GUI] Added missing GuiNotifier.

---
+++ Eduras/src/de/illonis/eduras/gameclient/gui/GuiNotifier.java
@@ -0,0 +1,101 @@
+package de.illonis.eduras.gameclient.gui;
+
+import java.util.ArrayList;
+
+import de.illonis.eduras.events.ClientRenameEvent;
+import de.illonis.eduras.events.GameEvent;
+import de.illonis.eduras.events.MatchEndEvent;
+import de.illonis.eduras.events.ObjectFactoryEvent;
+import de.illonis.eduras.events.SetGameObjectAttributeEvent;
+import de.illonis.eduras.events.SetItemSlotEvent;
+import de.illonis.eduras.events.SetOwnerEvent;
+import de.illonis.eduras.gameclient.gui.guielements.RenderedGuiObject;
+import de.illonis.eduras.gamemodes.GameMode;
+import de.illonis.eduras.gameobjects.GameObject;
+import de.illonis.eduras.interfaces.GameEventListener;
+
+public class GuiNotifier implements GameEventListener {
+	private ArrayList<RenderedGuiObject> uiObjects;
+
+	public GuiNotifier(ArrayList<RenderedGuiObject> uiObjects) {
+		this.uiObjects = uiObjects;
+	}
+
+	@Override
+	public void onNewObjectPosition(GameObject object) {
+	}
+
+	@Override
+	public void onInformationRequested(ArrayList<GameEvent> infos,
+			int targetOwner) {
+	}
+
+	@Override
+	public void onObjectCreation(ObjectFactoryEvent event) {
+		for (RenderedGuiObject obj : uiObjects) {
+			obj.onObjectCreation(event);
+		}
+	}
+
+	@Override
+	public void onClientRename(ClientRenameEvent event) {
+		for (RenderedGuiObject obj : uiObjects) {
+			obj.onClientRename(event);
+		}
+	}
+
+	@Override
+	public void onObjectStateChanged(SetGameObjectAttributeEvent<?> event) {
+		for (RenderedGuiObject obj : uiObjects) {
+			obj.onObjectStateChanged(event);
+		}
+	}
+
+	@Override
+	public void onGameModeChanged(GameMode newGameMode) {
+		for (RenderedGuiObject obj : uiObjects) {
+			obj.onGameModeChanged(newGameMode);
+		}
+	}
+
+	@Override
+	public void onHealthChanged(int objectId, int newValue) {
+		for (RenderedGuiObject obj : uiObjects) {
+			obj.onHealthChanged(objectId, newValue);
+		}
+	}
+
+	@Override
+	public void onOwnerChanged(SetOwnerEvent event) {
+		for (RenderedGuiObject obj : uiObjects) {
+			obj.onOwnerChanged(event);
+		}
+	}
+
+	@Override
+	public void onItemSlotChanged(SetItemSlotEvent event) {
+		for (RenderedGuiObject obj : uiObjects) {
+			obj.onItemSlotChanged(event);
+		}
+	}
+
+	@Override
+	public void onObjectRemove(ObjectFactoryEvent event) {
+	}
+
+	@Override
+	public void onMatchEnd(MatchEndEvent event) {
+		for (RenderedGuiObject obj : uiObjects) {
+			obj.onMatchEnd(event);
+		}
+	}
+
+	/**
+	 * Notifies gui objects that player object has been received.
+	 */
+	public void onPlayerReceived() {
+		for (RenderedGuiObject obj : uiObjects) {
+			obj.onPlayerInformationReceived();
+		}
+	}
+}
