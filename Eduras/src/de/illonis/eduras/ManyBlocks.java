3c4a0d3e9373b045bddf8812502f95f890277f04
Florian Mai <florian.ren.mai@googlemail.com>
2013-05-12 17:59:57 +0200

    [MAPS] Introduced new example map.

---
+++ Eduras/src/de/illonis/eduras/ManyBlocks.java
@@ -0,0 +1,43 @@
+package de.illonis.eduras;
+
+import java.util.LinkedList;
+
+import de.illonis.eduras.exceptions.ShapeVerticesNotApplicableException;
+import de.illonis.eduras.gameobjects.BigBlock;
+import de.illonis.eduras.gameobjects.GameObject;
+import de.illonis.eduras.logger.EduLog;
+
+public class ManyBlocks extends Map {
+
+	public ManyBlocks() {
+		super("manyblocks", 500, 500);
+
+		LinkedList<GameObject> initialObjects = new LinkedList<GameObject>();
+
+		int bigBlockWidth = 20;
+		int bigBlockHeight = 20;
+
+		final int space = 5;
+
+		for (int i = 0; i < getWidth() / (space * bigBlockWidth); i++) {
+			for (int j = 0; j < getHeight() / (space * bigBlockHeight); j++) {
+				try {
+					BigBlock bigBlock = new BigBlock(null, space * i
+							* bigBlockWidth, space * j * bigBlockHeight, -1);
+					initialObjects.add(bigBlock);
+				} catch (ShapeVerticesNotApplicableException e) {
+					EduLog.passException(e);
+				}
+			}
+		}
+
+		setInitialObjects(initialObjects);
+		System.out.println(initialObjects.size());
+
+		for (GameObject gameObject : initialObjects) {
+			System.out.println(gameObject.getDrawX());
+			System.out.println(gameObject.getDrawY());
+		}
+	}
+
+}
