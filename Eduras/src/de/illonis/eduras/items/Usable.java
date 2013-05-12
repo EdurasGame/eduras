d6b08684ac151280df343e76658e64d0a57e7230
illonis <things@illonis.de>
2013-05-12 17:10:59 +0200

    [ITEMS][USABLE] Added cooldown methods.

--- Eduras/src/de/illonis/eduras/items/Usable.java
+++ Eduras/src/de/illonis/eduras/items/Usable.java
@@ -1,14 +1,56 @@
 package de.illonis.eduras.items;
 
-
 /**
- * Indicates an item is usable.
+ * Indicates that an item is usable and has a cooldown.
  * 
  * @author illonis
  * 
  */
 public interface Usable {
 
+	/**
+	 * Triggers use of this item.
+	 * 
+	 * @param info
+	 *            usage information.
+	 * 
+	 * @author illonis
+	 */
 	public void use(ItemUseInformation info);
+
+	/**
+	 * Returns current cooldown of item.
+	 * 
+	 * @return current cooldown.
+	 * 
+	 * @author illonis
+	 */
+	public int getCooldown();
+
+	/**
+	 * Returns default cooldown time of this item.
+	 * 
+	 * @return default cooldown time.
+	 * 
+	 * @author illonis
+	 */
+	public int getCooldownTime();
+
+	/**
+	 * Reduces the cooldown of this item by given value.
+	 * 
+	 * @param value
+	 *            cooldown reducing in seconds.
+	 * 
+	 * @author illonis
+	 */
+	public void reduceCooldown(int value);
+
+	/**
+	 * Resets cooldown
+	 * 
+	 * @author illonis
+	 */
+	public void resetCooldown();
 
 }
