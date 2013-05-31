fca21799d98407cee69858a30d935dae742161d6
illonis <things@illonis.de>
2013-05-31 13:17:21 +0200

    [WEBSITE] Started website.

---
+++ Eduras/www/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  		adaptiveHeight: true,
+  		autoStart: false,
+		pagerCustom: $('#pageNav'),
+		mode: 'fade',
+		controls: false
+  	
+  });
+});
