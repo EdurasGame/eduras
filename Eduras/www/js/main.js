2b39a10ea284c64261e11cbab9cfe73322c8ad67
illonis <things@illonis.de>
2013-06-01 16:49:03 +0200

    [WEBSITE] Major graphics update and some content.

--- Eduras/www/js/main.js
+++ Eduras/www/js/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-  $('.bxslider').bxSlider({
+  var slider = $('.bxslider').bxSlider({
   		adaptiveHeight: true,
   		autoStart: false,
 		pagerCustom: $('#pageNav'),
 		mode: 'fade',
 		controls: false
-  	
   });
+  
+	$('a').on('click', function() {
+		this.blur();
+	});
+	
+	$('#impressum').on('click', function(event) {
+		slider.goToSlide(4);
+		event.preventDefault();
+	});
 });
