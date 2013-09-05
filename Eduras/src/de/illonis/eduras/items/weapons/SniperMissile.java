6796ae702d2bbdd5b25049d2976c59960d60c895
illonis <things@illonis.de>
2013-09-05 20:01:39 +0200

    [ITEMS][MISSILES] Refactored some things to reduce redundance.

--- Eduras/src/de/illonis/eduras/items/weapons/SniperMissile.java
+++ Eduras/src/de/illonis/eduras/items/weapons/SniperMissile.java
@@ -6,9 +6,7 @@ package de.illonis.eduras.items.weapons;
 import de.illonis.eduras.GameInformation;
 import de.illonis.eduras.ObjectFactory.ObjectType;
 import de.illonis.eduras.gameobjects.ArtificialIntelligence;
-import de.illonis.eduras.gameobjects.GameObject;
 import de.illonis.eduras.shapes.Circle;
-import de.illonis.eduras.units.Unit;
 
 /**
  * 
@@ -31,19 +29,6 @@ public class SniperMissile extends Missile {
 		setObjectType(ObjectType.SNIPERMISSILE);
 		setShape(new Circle(1.5));
 		setSpeed(500);
-	}
-
-	@Override
-	public void onCollision(GameObject collidingObject) {
-		if (collidingObject.isUnit()) {
-			((Unit) collidingObject).damagedBy(getDamage(), getOwner());
-		}
-		removeSelf();
-	}
-
-	@Override
-	public void onMapBoundsReached() {
-		removeSelf();
 	}
 
 	@Override
