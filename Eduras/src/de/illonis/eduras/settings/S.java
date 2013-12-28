f13e8dd6c172040b96f8e9a4456279b883a549fe
Florian Mai <florian.ren.mai@googlemail.com>
2013-12-28 11:21:26 -0800

    [SETTINGS][S-CLASS] Implemented loading settings from file and tested it.

--- Eduras/src/de/illonis/eduras/settings/S.java
+++ Eduras/src/de/illonis/eduras/settings/S.java
@@ -1,8 +1,12 @@
 package de.illonis.eduras.settings;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,7 +24,13 @@ public final class S {
 	private final static Logger L = EduLog.getLoggerFor(EdurasServer.class
 			.getName());
 
-	public static boolean myBoolean = false;
+	// these are testvalues only
+	private static boolean myBoolean = false;
+	private static int myInt = 42;
+	private static long myLong = 23;
+	private static String myString = "fourtytwo";
+	private static float myFloat = 42.42f;
+	private static double myDouble = 42.4242;
 
 	/**
 	 * Reads values from the file and writes them to the S classes attributes.
@@ -38,13 +48,19 @@ public final class S {
 	 *            be read successfully are
 	 */
 	public static void loadSettings(File file) {
-		String[][] readSettings = parseFileToSettings(file);
-		int numberOfReadSettings = readSettings.length;
+
+		if (file == null) {
+			L.warning("File must not be NULL!");
+			return;
+		}
+
+		LinkedList<String[]> readSettings = parseFileToSettings(file);
+		int numberOfReadSettings = readSettings.size();
 
 		int i;
 		for (i = 0; i < numberOfReadSettings; i++) {
-			String settingName = readSettings[i][0];
-			String settingValue = readSettings[i][1];
+			String settingName = readSettings.get(i)[0];
+			String settingValue = readSettings.get(i)[1];
 
 			Field setting;
 			try {
@@ -56,25 +72,80 @@ public final class S {
 			}
 
 			try {
+
 				if (setting.getType().equals(boolean.class)) {
 					setting.setBoolean(null, Boolean.parseBoolean(settingValue));
 				}
+				if (setting.getType().equals(String.class)) {
+					setting.set(null, settingValue);
+				}
+				if (setting.getType().equals(int.class)) {
+					setting.setInt(null, Integer.parseInt(settingValue));
+				}
+				if (setting.getType().equals(long.class)) {
+					setting.setLong(null, Long.parseLong(settingValue));
+				}
+				if (setting.getType().equals(double.class)) {
+					setting.setDouble(null, Double.parseDouble(settingValue));
+				}
+				if (setting.getType().equals(float.class)) {
+					setting.setFloat(null, Float.parseFloat(settingValue));
+				}
+
 			} catch (IllegalAccessException e) {
 				L.log(Level.SEVERE, "Error while trying to set setting "
-						+ settingName + " to value " + settingValue);
+						+ settingName + " to value " + settingValue, e);
+				continue;
+			} catch (IllegalArgumentException e) {
+				L.log(Level.WARNING,
+						"Format exception while trying to read the value "
+								+ settingValue + " of setting " + settingName,
+						e);
 				continue;
 			}
 		}
 
 	}
 
-	private static String[][] parseFileToSettings(File file) {
-		String[][] readSettings = new String[1][2];
-		readSettings[0][0] = "myBoolean";
-		readSettings[0][1] = "true";
+	private static LinkedList<String[]> parseFileToSettings(File file) {
+		LinkedList<String[]> readSettings = new LinkedList<String[]>();
+
+		BufferedReader fileReader;
+		try {
+			fileReader = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e1) {
+			L.log(Level.SEVERE, "Cannot find file", e1);
+			return readSettings;
+		}
+		String readLine = "";
+		try {
+			while ((readLine = fileReader.readLine()) != null) {
+				String[] settingNameAndValue = readLine.split("=");
+				if (settingNameAndValue.length == 2) {
+					readSettings.add(settingNameAndValue);
+				} else {
+					L.warning("Were not able to parse line " + readLine);
+					continue;
+				}
+			}
+		} catch (IOException e) {
+			L.log(Level.SEVERE,
+					"An IO error appeared trying to read settings file.", e);
+		}
+
+		try {
+			fileReader.close();
+		} catch (IOException e) {
+			L.log(Level.SEVERE, "Error trying to close the file.");
+		}
 		return readSettings;
 	}
 
+	/**
+	 * Tests the S class' fileloader.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		try {
 			EduLog.init("test.log");
@@ -82,9 +153,20 @@ public final class S {
 			e.printStackTrace();
 		}
 		System.out.println("Myboolean vorher: " + myBoolean);
+		System.out.println("MyLong vorher: " + myLong);
+		System.out.println("MyInt vorher: " + myInt);
+		System.out.println("MyString vorher: " + myString);
+		System.out.println("MyDouble vorher: " + myDouble);
+		System.out.println("MyFloat vorher: " + myFloat);
 
-		loadSettings(null);
+		loadSettings(new File(
+				"D:\\Reposses\\eduras\\Eduras\\src\\de\\illonis\\eduras\\unittests\\testsettingfile"));
 
-		System.out.println("Myboolean nachher: " + myBoolean);
+		System.out.println("MyBoolean nachher: " + myBoolean);
+		System.out.println("MyLong nachher: " + myLong);
+		System.out.println("MyInt nachher: " + myInt);
+		System.out.println("MyString nachher: " + myString);
+		System.out.println("MyDouble nachher: " + myDouble);
+		System.out.println("MyFloat nachher: " + myFloat);
 	}
 }
