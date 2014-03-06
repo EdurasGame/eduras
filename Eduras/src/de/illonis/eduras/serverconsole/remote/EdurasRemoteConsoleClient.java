e0847a1b347756fbce02fea1a703f59e125abd57
illonis <things@illonis.de>
2014-03-06 13:42:40 +0100

    [CONSOLE][REMOTE] Added Remote console client.

---
+++ Eduras/src/de/illonis/eduras/serverconsole/remote/EdurasRemoteConsoleClient.java
@@ -0,0 +1,126 @@
+package de.illonis.eduras.serverconsole.remote;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+import de.eduras.remote.EncryptedRemoteClient;
+import de.eduras.remote.RemoteException;
+import de.eduras.remote.RemoteServer;
+import de.eduras.remote.security.EncryptionService;
+import de.illonis.edulog.EduLog;
+
+public class EdurasRemoteConsoleClient extends EncryptedRemoteClient {
+
+	private boolean authenticated;
+	private final BufferedReader console;
+	private RemoteConsoleClientFrame frame;
+
+	public EdurasRemoteConsoleClient() {
+		authenticated = false;
+		frame = new RemoteConsoleClientFrame(this);
+		console = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+	public void showGui() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				frame.show();
+			}
+		}).start();
+		frame.appendOutput("Waiting for connection details...");
+	}
+
+	public void askConnection() {
+		String s = (String) JOptionPane.showInputDialog(null,
+				"Enter server address:\n" + "\"e.g. 192.168.0.4:4441",
+				"Connect to remote console", JOptionPane.PLAIN_MESSAGE, null,
+				null, "localhost:" + RemoteServer.DEFAULT_REMOTE_SERVER_PORT);
+
+		// If a string was returned, say so.
+		if ((s == null) || (s.length() <= 0)) {
+			System.exit(0);
+			return;
+		}
+
+		String[] vals = s.split(":");
+		InetAddress addr;
+		try {
+			addr = InetAddress.getByName(vals[0]);
+			int port = Integer.parseInt(vals[1]);
+			frame.appendOutput("Connecting...");
+			if (connect(addr, port)) {
+				frame.appendOutput("Connected.");
+				frame.appendOutput("Enter password:");
+				frame.ready();
+			} else {
+				frame.appendOutput("Connecting failed.");
+			}
+		} catch (UnknownHostException | NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Invalid address entered.",
+					"Error", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		EdurasRemoteConsoleClient client = new EdurasRemoteConsoleClient();
+		client.showGui();
+		client.askConnection();
+	}
+
+	@Override
+	public void onAuthenticationSuccessed() {
+		frame.appendOutput("Authentication successed.");
+	}
+
+	@Override
+	public void onAuthenticationFailed() {
+		frame.appendOutput("Auth failed.");
+		frame.appendOutput("Enter password:");
+	}
+
+	@Override
+	public void onAnswer(String answer) {
+		frame.appendOutput(answer);
+	}
+
+	@Override
+	public void onDisconnected() {
+		frame.appendOutput("disconnected");
+		System.exit(0);
+	}
+
+	@Override
+	public void onConnectionLost() {
+		frame.appendOutput("connection lost");
+		System.exit(1);
+	}
+
+	public void onCommand(String command) throws RemoteException {
+		if (command.equals("exit")) {
+			disconnect();
+			return;
+		}
+		if (!isAuthenticated()) {
+			// if we are prompting for a password, we must encrypt it!
+			try {
+				command = EncryptionService.oneWayEncrypt(command);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+		sendCommand(command);
+	}
+
+}
---
+++ Eduras/src/de/illonis/eduras/serverconsole/remote/RemoteConsoleClientFrame.java
@@ -0,0 +1,93 @@
+package de.illonis.eduras.serverconsole.remote;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import de.eduras.remote.RemoteException;
+
+public class RemoteConsoleClientFrame implements ActionListener {
+
+	private JFrame frame;
+	private JTextArea consoleOutput;
+	private JTextField inputField;
+	private JButton sendButton;
+	private final EdurasRemoteConsoleClient client;
+	private final static String PROMPT = "eduras-remote>";
+
+	public RemoteConsoleClientFrame(EdurasRemoteConsoleClient client) {
+		this.client = client;
+		initGui();
+	}
+
+	private void initGui() {
+		frame = new JFrame("RemoteClient");
+		frame.setSize(600, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		consoleOutput = new JTextArea();
+		consoleOutput.setLineWrap(true);
+		consoleOutput.setEditable(false);
+		consoleOutput.setWrapStyleWord(true);
+		consoleOutput.setBackground(Color.BLACK);
+		consoleOutput.setForeground(Color.WHITE);
+		consoleOutput.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+		JPanel content = (JPanel) frame.getContentPane();
+		content.setLayout(new BorderLayout());
+		JScrollPane areaScrollPane = new JScrollPane(consoleOutput);
+		areaScrollPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		areaScrollPane
+				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		content.add(areaScrollPane, BorderLayout.CENTER);
+
+		JPanel bottomPanel = new JPanel(new BorderLayout());
+		inputField = new JTextField();
+		JLabel label = new JLabel(PROMPT);
+		bottomPanel.add(label, BorderLayout.WEST);
+		inputField.addActionListener(this);
+		sendButton = new JButton("send");
+		sendButton.addActionListener(this);
+		bottomPanel.add(inputField, BorderLayout.CENTER);
+		bottomPanel.add(sendButton, BorderLayout.EAST);
+		content.add(bottomPanel, BorderLayout.SOUTH);
+		sendButton.setEnabled(false);
+		inputField.setEnabled(false);
+	}
+
+	public void show() {
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String command = inputField.getText();
+		inputField.setText("");
+		try {
+			client.onCommand(command);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void appendOutput(String text) {
+		consoleOutput.append(text + System.lineSeparator());
+		consoleOutput.setCaretPosition(consoleOutput.getDocument().getLength());
+	}
+
+	public void ready() {
+		sendButton.setEnabled(true);
+		inputField.setEnabled(true);
+		inputField.requestFocus();
+	}
+
+}
