5c949f127354318793514a346b5bf730ed1fc6de
Florian 'Ren' Mai <florian.ren.mai@googlemail.com>
2014-03-17 05:02:37 +0100

    [CLEANUP][DOCU] Added javadoc for fields in S.

--- Eduras/src/de/illonis/eduras/settings/S.java
+++ Eduras/src/de/illonis/eduras/settings/S.java
@@ -12,6 +12,26 @@ import java.util.logging.Logger;
 
 import de.illonis.edulog.EduLog;
 import de.illonis.eduras.EdurasServer;
+import de.illonis.eduras.gameobjects.BigBlock;
+import de.illonis.eduras.gameobjects.BiggerBlock;
+import de.illonis.eduras.gameobjects.Bird;
+import de.illonis.eduras.items.weapons.AssaultMissile;
+import de.illonis.eduras.items.weapons.AssaultRifle;
+import de.illonis.eduras.items.weapons.MineMissile;
+import de.illonis.eduras.items.weapons.MineWeapon;
+import de.illonis.eduras.items.weapons.Missile;
+import de.illonis.eduras.items.weapons.RocketLauncher;
+import de.illonis.eduras.items.weapons.RocketMissile;
+import de.illonis.eduras.items.weapons.SimpleMissile;
+import de.illonis.eduras.items.weapons.SimpleWeapon;
+import de.illonis.eduras.items.weapons.SniperMissile;
+import de.illonis.eduras.items.weapons.SniperWeapon;
+import de.illonis.eduras.items.weapons.SplashMissile;
+import de.illonis.eduras.items.weapons.SplashWeapon;
+import de.illonis.eduras.items.weapons.SplashedMissile;
+import de.illonis.eduras.items.weapons.SwordMissile;
+import de.illonis.eduras.items.weapons.SwordWeapon;
+import de.illonis.eduras.units.PlayerMainFigure;
 
 /**
  * This class holds all flexible settings.
@@ -25,156 +45,402 @@ public final class S {
 			.getName());
 
 	// real values
+	/**
+	 * Max distance of an AI object to its destination at which the target is
+	 * assumed to be reached.
+	 */
 	public static double ai_target_reached_distance = 10;
 
+	/**
+	 * Denotes the interval in which an AI object's motion behavior is updated
+	 * by the AI.
+	 */
 	public static long ai_motion_update_interval = 400;
 
+	/**
+	 * Denotes the width of a {@link BigBlock} object.
+	 */
 	public static int go_big_block_width = 20;
 
+	/**
+	 * Denotes the height of a {@link BigBlock} object.
+	 */
 	public static int go_big_block_height = 20;
 
+	/**
+	 * Denotes the width of a {@link BiggerBlock} object.
+	 */
 	public static int go_bigger_block_width = 40;
 
+	/**
+	 * Denotes the height of a {@link BiggerBlock} object.
+	 */
 	public static int go_bigger_block_height = 40;
 
+	/**
+	 * The speed of a {@link Bird} object.
+	 */
 	public static double go_bird_speed = 40;
 
+	/**
+	 * Tells whether a {@link Bird} is collidable or not.
+	 */
 	public static boolean go_bird_collidable = false;
 
+	/**
+	 * Max number of items a {@link PlayerMainFigure} can carry.
+	 */
 	public static int player_max_item_capacity = 6;
 
+	/**
+	 * Denotes the size of a {@link Missile} object.
+	 */
 	public static double go_missile_radius = 5;
 
+	/**
+	 * Damage caused by one {@link SimpleMissile} object.
+	 */
 	public static int go_simplemissile_damage = 5;
 
+	/**
+	 * Area Of Effect of a {@link SimpleMissile} object.
+	 */
 	public static double go_simplemissile_damage_radius = 5;
 
+	/**
+	 * Speed of a {@link SimpleMissile} object.
+	 */
 	public static double go_simplemissile_speed = 100;
 
+	/**
+	 * Denotes how far a {@link SimpleMissile} object can go before it
+	 * disappears.
+	 */
 	public static double go_simplemissile_maxrange = 200;
 
+	/**
+	 * Denotes the size of a {@link SimpleWeapon} object.
+	 */
 	public static double go_simpleweapon_shape_radius = 10;
 
+	/**
+	 * Cooldown of a {@link SimpleWeapon}.
+	 */
 	public static long go_simpleweapon_cooldown = 300;
 
+	/**
+	 * Damage caused by a {@link SniperMissile}.
+	 */
 	public static int go_snipermissile_damage = 16;
 
+	/**
+	 * Area of Effect of a {@link SniperMissile}.
+	 */
 	public static double go_snipermissile_damageradius = 1;
 
+	/**
+	 * Radius of the shape of a {@link SniperMissile} object.
+	 */
 	public static double go_snipermissile_shape_radius = 1.5;
 
+	/**
+	 * Speed of a {@link SniperMissile}.
+	 */
 	public static double go_snipermissile_speed = 500;
 
+	/**
+	 * Size (radius) of a {@link SniperWeapon} object.
+	 */
 	public static double go_sniperweapon_shape_radius = 5;
 
+	/**
+	 * Cooldown of {@link SniperWeapon} item.
+	 */
 	public static long go_sniperweapon_cooldown = 1050;
 
+	/**
+	 * Damage caused by one {@link SplashedMissile} object.
+	 */
 	public static int go_splashedmissile_damage = 3;
 
+	/**
+	 * Area of Effect of {@link SplashedMissile}.
+	 */
 	public static double go_splashedmissile_damageradius = 1;
 
+	/**
+	 * Size (radius) of {@link SplashedMissile}.
+	 */
 	public static double go_splashedmissile_shape_radius = 3;
 
+	/**
+	 * Speed of a {link SplashedMissile}.
+	 */
 	public static double go_splashedmissile_speed = 250;
 
+	/**
+	 * Damage caused by a {@link SplashMissile}.
+	 */
 	public static int go_splashmissile_damage = 7;
 
+	/**
+	 * Area of Effect of {@link SplashMissile}.
+	 */
 	public static double go_splashmissile_damageradius = 1;
 
+	/**
+	 * Size (radius) of {@link SplashMissile}.
+	 */
 	public static double go_splashmissile_shape_radius = 5;
 
+	/**
+	 * Speed of a {link SplashMissile}.
+	 */
 	public static double go_splashmissile_speed = 250;
 
+	/**
+	 * Size (radius) of a {link SplashWeapon}.
+	 */
 	public static double go_splashweapon_shape_radius = 7;
 
+	/**
+	 * Denotes the cooldown when you use a {@link SplashWeapon}.
+	 */
 	public static long go_splashweapon_cooldown = 900;
 
+	/**
+	 * Damage caused by a {@link SwordMissile}.
+	 */
 	public static int go_swordmissile_damage = 2;
 
+	/**
+	 * Area of Effect of {@link SwordMissile}.
+	 */
 	public static double go_swordmissile_damageradius = 1;
 
+	/**
+	 * Speed of {@link SwordMissile}.
+	 */
 	public static double go_swordmissile_speed = 100;
 
+	/**
+	 * Denotes how far a {@link SwordMissile} can go before it disappears.
+	 */
 	public static double go_swordmissile_maxrange = 10;
 
+	/**
+	 * Size (radius) of {@link SwordMissile}.
+	 */
 	public static double go_swordweapon_shape_radius = 7;
 
+	/**
+	 * Denotes the cooldown of the {@link SwordWeapon} before you can use it
+	 * again.
+	 */
 	public static long go_swordweapon_cooldown = 100;
 
+	/**
+	 * Denotes the default respawn time of all weapons.
+	 */
 	public static long go_weapon_respawntime_default = 5000;
 
+	/**
+	 * Says how much health a {@link PlayerMainFigure} can have at maximum.
+	 */
 	public static int player_maxhealth_default = 30;
 
+	/**
+	 * Base speed of a {@link PlayerMainFigure}.
+	 */
 	public static double player_speed_default = 50;
 
+	/**
+	 * Tells in how many {@link SplashedMissile}s a {@link SplashMissile} breaks
+	 * after hitting something.
+	 */
 	public static int go_splashmissile_splinters = 10;
 
+	/**
+	 * Rotation gets updated only when it has changed by an angle at least this
+	 * big.
+	 */
 	public static double sv_performance_rotationdelta_minimum = 5;
 
+	/**
+	 * Denotes the cooldown of a {@link RocketLauncher}.
+	 */
 	public static long go_rocketlauncher_cooldown = 3000;
 
+	/**
+	 * Area Of Effect of a {@link RocketMissile}.
+	 */
 	public static double go_rocketmissile_damageradius = 100;
 
+	/**
+	 * Damage caused by a {@link RocketMissile}.
+	 */
 	public static int go_rocketmissile_damage = 12;
 
+	/**
+	 * Speed of a {@link RocketMissile}.
+	 */
 	public static double go_rocketmissile_speed = 150;
 
+	/**
+	 * Denotes how far a {@link RocketMissile} can go before it disappears.
+	 */
 	public static double go_rocketmissile_maxrange = 400;
 
+	/**
+	 * Size (radius) of a {@link MineWeapon}.
+	 */
 	public static double go_mineweapon_size = 5;
 
+	/**
+	 * Cooldown of {@link MineWeapon}.
+	 */
 	public static long go_mineweapon_cooldown = 500;
 
+	/**
+	 * Damage caused by a single {@link MineMissile}.
+	 */
 	public static int go_minemissile_damage = 6;
 
+	/**
+	 * Area Of Effect of a {@link MineMissile}.
+	 */
 	public static double go_minemissile_damageradius = 50;
 
+	/**
+	 * Speed of a {@link MineMissile}.
+	 */
 	public static double go_minemissile_speed = 0;
 
+	/**
+	 * Denotes how far a {@link MineMissile} can go before it disappears.
+	 */
 	public static double go_minemissile_maxrange = 1;
 
+	/**
+	 * Size (radius) of a {@link MineMissile}.
+	 */
 	public static double go_minemissile_shape_size = 2;
 
+	/**
+	 * Denotes by how much the {@link SplashWeapon}'s ammunition is filled up
+	 * when collecting it.
+	 */
 	public static int go_splashweapon_fillamount = 5;
 
+	/**
+	 * Denotes the upper limit of how many bullets of a {@link SplashWeapon} can
+	 * be carried.
+	 */
 	public static int go_splashweapon_maxammo = 50;
 
+	/**
+	 * Denotes by how much the {@link SniperWeapon}'s ammunition is filled up
+	 * when collecting it.
+	 */
 	public static int go_sniperweapon_fillamount = 5;
 
+	/**
+	 * Denotes the upper limit of how many bullets of a {@link SniperWeapon} can
+	 * be carried.
+	 */
 	public static int go_sniperweapon_maxammo = 20;
 
+	/**
+	 * Denotes by how much the {@link RocketLauncher}'s ammunition is filled up
+	 * when collecting it.
+	 */
 	public static int go_rocketlauncher_fillamount = 6;
 
+	/**
+	 * Denotes the upper limit of how many bullets of a {@link RocketLauncher}
+	 * can be carried.
+	 */
 	public static int go_rocketlauncher_maxammo = 60;
 
+	/**
+	 * Denotes by how much the {@link MineWeapon}'s ammunition is filled up when
+	 * collecting it.
+	 */
 	public static int go_mineweapon_fillamount = 3;
 
+	/**
+	 * Denotes the upper limit of how many bullets of a {@link MineWeapon} can
+	 * be carried.
+	 */
 	public static int go_mineweapon_maxammo = 30;
 
+	/**
+	 * Denotes by how much the {@link SimpleWeapon}'s ammunition is filled up
+	 * when collecting it.
+	 */
 	public static int go_simpleweapon_fillamount = 25;
 
+	/**
+	 * Denotes the upper limit of how many bullets of a {@link SimpleWeapon} can
+	 * be carried.
+	 */
 	public static int go_simpleweapon_maxammo = 200;
 
+	/**
+	 * Size (radius) of an {@link AssaultRifle}.
+	 */
 	public static double go_assaultrifle_shape_size = 5;
 
+	/**
+	 * Cooldown of the {@link AssaultRifle}.
+	 */
 	public static long go_assaultrifle_cooldown = 200;
 
+	/**
+	 * Denotes by how much the {@link AssaultRifle}'s ammunition is filled up
+	 * when collecting it.
+	 */
 	public static int go_assaultrifle_fillamount = 50;
 
+	/**
+	 * Denotes the upper limit of how many bullets of an {@link AssaultRifle}
+	 * can be carried.
+	 */
 	public static int go_assaultrifle_maxammo = 250;
 
+	/**
+	 * Damage caused by an {@link AssaultMissile}.
+	 */
 	public static int go_assaultmissile_damage = 2;
 
+	/**
+	 * Speed of an {@link AssaultMissile}.
+	 */
 	public static double go_assaultmissile_speed = 2000;
 
+	/**
+	 * Denotes how far an {@link AssaultMissile} can go before it disappears.
+	 */
 	public static double go_assaultmissile_maxrange = 4000;
 
+	/**
+	 * Size (radius) of an {@link AssaultMissile}.
+	 */
 	public static double go_assaultmissile_shape_size = 1;
 
+	/**
+	 * Area Of Effect of an {@link AssaultMissile}.
+	 */
 	public static double go_assaultmissile_damageradius = 3;
 
+	/**
+	 * Tells whether neutral objects are always shown when vision is enabled.
+	 */
 	public static boolean vision_neutral_always = true;
 
+	/**
+	 * Tells whether vision is disabled or not.
+	 */
 	public static boolean vision_disabled = true;
 
 	/**
