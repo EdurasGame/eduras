90d82c25c8963ba388196c51d56f5676c2d014fa
illonis <things@illonis.de>
2014-03-20 08:30:07 +0100

    [VERSION] Fixed a bug where version file was not included an a resulting NullPointerException was not catched.

--- Eduras/src/de/illonis/eduras/EdurasVersion.java
+++ Eduras/src/de/illonis/eduras/EdurasVersion.java
@@ -1,9 +1,10 @@
 package de.illonis.eduras;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,6 +19,7 @@ import de.illonis.edulog.EduLog;
 public class EdurasVersion {
 	private final static Logger L = EduLog.getLoggerFor(EdurasVersion.class
 			.getName());
+	private final static Charset ENCODING = StandardCharsets.UTF_8;
 
 	/**
 	 * Returns the Eduras? distribution's version.
@@ -26,25 +28,25 @@ public class EdurasVersion {
 	 */
 	public static String getVersion() {
 		String version = "unknown";
-
-		File versionFile = new File(System.class.getResource("/version")
-				.getPath());
-		BufferedReader fileReader = null;
-		try {
-			fileReader = new BufferedReader(new FileReader(versionFile));
-			version = fileReader.readLine();
+		URL res = EdurasVersion.class.getResource("/version");
+		if (res == null) {
+			L.log(Level.SEVERE, "Could not locate version file in resources.");
+			return version;
+		}
+		// using a try-with-resource automatically closes this resource and
+		// limits code chaos.
+		// see
+		// http://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+		try (Scanner scanner = new Scanner(res.openStream(), ENCODING.name())) {
+			if (scanner.hasNextLine())
+				version = scanner.nextLine();
+			else
+				L.log(Level.SEVERE, "Version file is empty.");
 		} catch (IOException e) {
 			L.log(Level.SEVERE, "Cannot find or read version file.", e);
-			version = "unknown";
-		} finally {
-			if (fileReader != null) {
-				try {
-					fileReader.close();
-				} catch (IOException e) {
-					L.log(Level.WARNING, "Cannot close file reader.", e);
-				}
-			}
 		}
+
+		L.log(Level.INFO, "Resolved current version to " + version);
 
 		return version;
 	}
