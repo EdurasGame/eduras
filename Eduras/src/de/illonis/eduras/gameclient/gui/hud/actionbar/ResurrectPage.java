0329c6e3af96365972820ea541bd970f8dc74daa
Florian 'Ren' Mai <florian.ren.mai@googlemail.com>
2014-05-05 16:13:33 +0200

    [GUI][RTSACTIONS] Resurrect page displays all players now (bug #81).

--- Eduras/src/de/illonis/eduras/gameclient/gui/hud/actionbar/ResurrectPage.java
+++ Eduras/src/de/illonis/eduras/gameclient/gui/hud/actionbar/ResurrectPage.java
@@ -46,7 +46,17 @@ public class ResurrectPage extends ActionBarPage {
 
 	@Override
 	public void onGameReady() {
-		// TODO: add all existing players.
+		InformationProvider infoPro = EdurasInitializer.getInstance()
+				.getInformationProvider();
+
+		for (Player player : infoPro.getPlayers()) {
+			try {
+				addRezzButtonForPlayer(player);
+			} catch (ObjectNotFoundException e) {
+				L.log(Level.SEVERE, "Joined player cannot be found.", e);
+				continue;
+			}
+		}
 	}
 
 	@Override
@@ -73,17 +83,24 @@ public class ResurrectPage extends ActionBarPage {
 
 		try {
 			Player joinedPlayer = infoPro.getPlayerByOwnerId(ownerId);
-			Player player = infoPro.getPlayer();
-			if (joinedPlayer.getTeam().equals(player.getTeam())) {
-				RezzButton rezzButton = new RezzButton(joinedPlayer, reactor);
-				rezzButton.cacheReady();
-				addButton(rezzButton);
-				// refresh bar if it is currently visible
-				if (bar.getCurrentPage() == getId())
-					bar.setPage(getId());
-			}
+			addRezzButtonForPlayer(joinedPlayer);
 		} catch (ObjectNotFoundException e) {
 			L.log(Level.SEVERE, "Joined player cannot be found.", e);
+		}
+	}
+
+	private void addRezzButtonForPlayer(Player player)
+			throws ObjectNotFoundException {
+		InformationProvider infoPro = EdurasInitializer.getInstance()
+				.getInformationProvider();
+
+		if (player.getTeam().equals(infoPro.getPlayer().getTeam())) {
+			RezzButton rezzButton = new RezzButton(player, reactor);
+			rezzButton.cacheReady();
+			addButton(rezzButton);
+			// refresh bar if it is currently visible
+			if (bar.getCurrentPage() == getId())
+				bar.setPage(getId());
 		}
 	}
 
