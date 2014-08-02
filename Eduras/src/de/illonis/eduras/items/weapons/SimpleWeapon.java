667baa2d47abd7660f6cfb09be3811cfc58699f4
illonis <things@illonis.de>
2014-08-02 09:32:25 +0200

    [SIMPLE_WEAPON] Added shape and ingame-image for simple_weapon.

--- Eduras/src/de/illonis/eduras/items/weapons/SimpleWeapon.java
+++ Eduras/src/de/illonis/eduras/items/weapons/SimpleWeapon.java
@@ -1,12 +1,12 @@
 package de.illonis.eduras.items.weapons;
 
-import org.newdawn.slick.geom.Circle;
-
 import de.illonis.eduras.GameInformation;
 import de.illonis.eduras.ObjectFactory.ObjectType;
 import de.illonis.eduras.gameobjects.TimingSource;
 import de.illonis.eduras.items.ItemUseInformation;
 import de.illonis.eduras.settings.S;
+import de.illonis.eduras.shapes.ShapeFactory;
+import de.illonis.eduras.shapes.ShapeFactory.ShapeType;
 
 /**
  * @author Florian Mai <florian.ren.mai@googlemail.com>
@@ -27,9 +27,7 @@ public class SimpleWeapon extends Weapon {
 	public SimpleWeapon(GameInformation gi, TimingSource timingSource, int id) {
 		super(ObjectType.ITEM_WEAPON_SIMPLE, gi, timingSource, id);
 		setName("SimpleWeapon");
-		setShape(new Circle(S.Server.go_simpleweapon_shape_radius,
-				S.Server.go_simpleweapon_shape_radius,
-				S.Server.go_simpleweapon_shape_radius));
+		setShape(ShapeFactory.createShape(ShapeType.WEAPON_1));
 		defaultCooldown = S.Server.go_simpleweapon_cooldown;
 		setAmmunitionLimited(S.Server.go_simpleweapon_fillamount,
 				S.Server.go_simpleweapon_maxammo);
