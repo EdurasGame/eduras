382077707cdacbe24b40213a93447dffe528bf30
Florian 'Ren' Mai <florian.ren.mai@googlemail.com>
2014-08-07 13:57:03 +0200

    [GUI][HUD] A time frame is an abstract gui element now. A specialization needs to provide a method to retrieve the time to be displayed.

--- Eduras/src/de/illonis/eduras/gameclient/gui/hud/TimeFrame.java
+++ Eduras/src/de/illonis/eduras/gameclient/gui/hud/TimeFrame.java
@@ -4,25 +4,33 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 
 /**
- * A time frame that shows remaining time.
+ * A time frame that shows some indication of time on the right side of the gui
+ * in a minutes:seconds format.
  * 
- * @author illonis
+ * @author illonis / renmai
  * 
  */
-public class TimeFrame extends RenderedGuiObject {
+public abstract class TimeFrame extends RenderedGuiObject {
 	private final static int WIDTH = 40;
+
+	private final float yOffset;
+	private final Color stringColor;
 
 	/**
 	 * Creates a new time frame.
 	 * 
 	 * @param gui
 	 *            associated gui.
+	 * @param yOffset
+	 *            the offset of y position where to draw the time-string
 	 */
-	public TimeFrame(UserInterface gui) {
+	protected TimeFrame(UserInterface gui, Color stringColor, float yOffset) {
 		super(gui);
 		setVisibleForSpectator(true);
 		screenX = 0;
 		screenY = 0;
+		this.yOffset = yOffset;
+		this.stringColor = stringColor;
 	}
 
 	@Override
@@ -30,17 +38,19 @@ public class TimeFrame extends RenderedGuiObject {
 
 		String timeString = getRemainingTimeString();
 		int textWidth = g2d.getFont().getWidth(timeString);
-		g2d.setColor(Color.white);
+		g2d.setColor(stringColor);
 		g2d.drawString(getRemainingTimeString(), screenX + WIDTH - textWidth
-				- 5, screenY + 10);
+				- 5, screenY + yOffset);
 	}
 
 	private String getRemainingTimeString() {
-		long remaining = getInfo().getRemainingTime() / 1000;
+		long remaining = getTimeToDisplay() / 1000;
 		int minutes = (int) (remaining / 60);
 		int seconds = (int) (remaining - minutes * 60);
 		return String.format("%d:%02d", minutes, seconds);
 	}
+
+	protected abstract long getTimeToDisplay();
 
 	@Override
 	public void onGuiSizeChanged(int newWidth, int newHeight) {
