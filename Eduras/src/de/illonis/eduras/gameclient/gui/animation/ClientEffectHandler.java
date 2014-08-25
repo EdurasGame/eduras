2100f69decc5ddf4963eda30a464e868e3aea3f8
Florian 'Ren' Mai <florian.ren.mai@googlemail.com>
2014-08-25 13:11:03 +0200

    [CLIENT][SOUNDS] Added a sound that is played when a base is conquered. Avoid playing sounds when map starts by waiting for the startround / gameready events.

--- Eduras/src/de/illonis/eduras/gameclient/gui/animation/ClientEffectHandler.java
+++ Eduras/src/de/illonis/eduras/gameclient/gui/animation/ClientEffectHandler.java
@@ -12,6 +12,7 @@ import de.illonis.eduras.Player;
 import de.illonis.eduras.Team;
 import de.illonis.eduras.events.ItemUseFailedEvent;
 import de.illonis.eduras.events.ItemUseFailedEvent.Reason;
+import de.illonis.eduras.events.MatchEndEvent;
 import de.illonis.eduras.events.ObjectFactoryEvent;
 import de.illonis.eduras.events.SetItemSlotEvent;
 import de.illonis.eduras.exceptions.ObjectNotFoundException;
@@ -41,28 +42,33 @@ public class ClientEffectHandler extends GameEventAdapter {
 
 	private final InformationProvider infos;
 
+	private boolean playSounds;
+
 	/**
 	 * Creates a new effect handler.
 	 */
 	public ClientEffectHandler() {
+		playSounds = false;
 		infos = EdurasInitializer.getInstance().getInformationProvider();
 		objectEffects = new HashMap<Integer, ConfigurableEmitter>();
 	}
 
 	@Override
 	public void onHealthChanged(Unit unit, int oldValue, int newValue) {
-		try {
-			if (unit.equals(infos.getPlayer().getPlayerMainFigure())) {
-				if (newValue > oldValue) {
-					SoundMachine.play(SoundType.LOOT);
-				} else {
-					if (newValue < oldValue) {
-						SoundMachine.play(SoundType.HURT);
+		if (playSounds) {
+			try {
+				if (unit.equals(infos.getPlayer().getPlayerMainFigure())) {
+					if (newValue > oldValue) {
+						SoundMachine.play(SoundType.LOOT);
+					} else {
+						if (newValue < oldValue) {
+							SoundMachine.play(SoundType.HURT);
+						}
 					}
 				}
+			} catch (ObjectNotFoundException e) {
+				L.log(Level.WARNING, "Cannot find player main figure.", e);
 			}
-		} catch (ObjectNotFoundException e) {
-			L.log(Level.WARNING, "Cannot find player main figure.", e);
 		}
 	}
 
@@ -90,21 +96,39 @@ public class ClientEffectHandler extends GameEventAdapter {
 
 	@Override
 	public void onBaseConquered(Base base, Team conqueringTeam) {
-		// TODO: Change sound to some special sound.
-		SoundMachine.play(SoundType.CLICK);
+		if (playSounds) {
+			SoundMachine.play(SoundType.TADA);
+		}
+	}
+
+	@Override
+	public void onGameReady() {
+		playSounds = true;
+	}
+
+	@Override
+	public void onMatchEnd(MatchEndEvent event) {
+		playSounds = false;
 	}
 
 	@Override
 	public void onItemSlotChanged(SetItemSlotEvent event) {
 
-		try {
-			Player player = infos.getPlayer();
-			if (event.getOwner() == player.getPlayerId()) {
-				player.getInventory().getItemBySlot(event.getItemSlot());
-				SoundMachine.play(SoundType.LOOT, 1f, .8f);
+		if (playSounds) {
+			try {
+				Player player = infos.getPlayer();
+				if (event.getOwner() == player.getPlayerId()) {
+					player.getInventory().getItemBySlot(event.getItemSlot());
+					SoundMachine.play(SoundType.LOOT, 1f, .8f);
+				}
+			} catch (ItemSlotIsEmptyException | ObjectNotFoundException e) {
+				// item lost or something like that
 			}
-		} catch (ItemSlotIsEmptyException | ObjectNotFoundException e) {
-			// item lost or something like that
 		}
+	}
+
+	@Override
+	public void onStartRound() {
+		playSounds = true;
 	}
 }
