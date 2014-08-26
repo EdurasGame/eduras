118813b0775ddfd5b49512b4563a36a56a971d85
illonis <jannis.mell@googlemail.com>
2014-08-26 14:08:55 +0200

    [HUD] Fixed Exitpopup location.

--- Eduras/src/de/illonis/eduras/gameclient/gui/hud/ExitPopup.java
+++ Eduras/src/de/illonis/eduras/gameclient/gui/hud/ExitPopup.java
@@ -5,7 +5,6 @@ import org.newdawn.slick.Font;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.Rectangle;
 
-import de.illonis.eduras.gameclient.datacache.CacheException;
 import de.illonis.eduras.gameclient.datacache.FontCache;
 import de.illonis.eduras.gameclient.datacache.FontCache.FontKey;
 
@@ -21,12 +20,10 @@ public class ExitPopup extends ClickableGuiElement {
 	private final Rectangle yesRect;
 	private final Rectangle noRect;
 	private final String text = "Do you really want to exit?";
-
-	UserInterface ui;
+	private Font font;
 
 	protected ExitPopup(UserInterface gui) {
 		super(gui);
-		this.ui = gui;
 		bounds = new Rectangle(0, 0, 300, 60);
 		yesRect = new Rectangle(0, 0, 100, 30);
 		noRect = new Rectangle(0, 0, 100, 30);
@@ -57,8 +54,23 @@ public class ExitPopup extends ClickableGuiElement {
 	@Override
 	public void render(Graphics g) {
 		if (isVisible()) {
-			Font font = FontCache.getFont(FontKey.DEFAULT_FONT, g);
-
+			if (font == null) {
+				font = FontCache.getFont(FontKey.DEFAULT_FONT, g);
+				yesRect.setSize(font.getWidth("Yes") + 10,
+						font.getLineHeight() + 10);
+				noRect.setSize(font.getWidth("Yes") + 10,
+						font.getLineHeight() + 10);
+				int width = Math.max((int) yesRect.getWidth(),
+						font.getWidth(text)) + 30;
+				bounds.setWidth(width);
+				int height = font.getLineHeight() * 2 + 30;
+				bounds.setHeight(height);
+				bounds.setLocation(screenX, screenY);
+				yesRect.setLocation(screenX + 10,
+						screenY + height - yesRect.getHeight() - 10);
+				noRect.setLocation(screenX + width - noRect.getWidth() - 10,
+						screenY + height - noRect.getHeight() - 10);
+			}
 			g.setColor(Color.white);
 			g.fill(bounds);
 			g.setColor(Color.black);
@@ -73,27 +85,8 @@ public class ExitPopup extends ClickableGuiElement {
 
 	@Override
 	public void onGuiSizeChanged(int newWidth, int newHeight) {
-		try {
-			Font font = FontCache.getFont(FontKey.DEFAULT_FONT);
-			yesRect.setSize(font.getWidth("Yes") + 10,
-					font.getLineHeight() + 10);
-			noRect.setSize(font.getWidth("Yes") + 10, font.getLineHeight() + 10);
-			int width = Math.max((int) yesRect.getWidth(), font.getWidth(text)) + 30;
-			bounds.setWidth(width);
-			int height = font.getLineHeight() * 2 + 30;
-			bounds.setHeight(height);
-			screenX = (newWidth - width) / 2;
-			screenY = (newHeight - height) / 2;
-			bounds.setLocation(screenX, screenY);
-			yesRect.setLocation(screenX + 10,
-					screenY + height - yesRect.getHeight() - 10);
-			noRect.setLocation(screenX + width - noRect.getWidth() - 10,
-					screenY + height - noRect.getHeight() - 10);
-
-		} catch (CacheException e) {
-			return;
-		}
-
+		screenX = (newWidth - bounds.getWidth()) / 2;
+		screenY = (newHeight - bounds.getHeight()) / 2;
 	}
 
 }
