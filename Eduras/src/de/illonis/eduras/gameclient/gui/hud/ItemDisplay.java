1d9d30c97dcea9e27e4728bdf67a35526be0d4bb
illonis <things@illonis.de>
2014-09-02 14:22:12 +0200

    [HUD] Changed item display to be left and made font smaller.

--- Eduras/src/de/illonis/eduras/gameclient/gui/hud/ItemDisplay.java
+++ Eduras/src/de/illonis/eduras/gameclient/gui/hud/ItemDisplay.java
@@ -9,7 +9,6 @@ import org.newdawn.slick.Font;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Rectangle;
-import org.newdawn.slick.geom.Vector2f;
 
 import de.illonis.edulog.EduLog;
 import de.illonis.eduras.events.SetItemSlotEvent;
@@ -32,8 +31,7 @@ import de.illonis.eduras.units.InteractMode;
  * @author illonis
  * 
  */
-public class ItemDisplay extends ClickableGuiElement implements
-		TooltipTriggerer {
+public class ItemDisplay extends RenderedGuiObject {
 
 	private final static Logger L = EduLog.getLoggerFor(ItemDisplay.class
 			.getName());
@@ -46,23 +44,17 @@ public class ItemDisplay extends ClickableGuiElement implements
 	private final static float ITEM_ALPHA = 0.5f;
 	private final static Color COLOR_MULTIPLIER = new Color(1f, 1f, 1f,
 			ITEM_ALPHA);
+	private final static Color FONT_BACKGROUND = new Color(0f, 0f, 0f, .7f);
+	private final static int ITEM_GAP = 5;
+	private final static int BLOCKSIZE = 36;
+	private final static int FONT_PADDING = 2;
 
-	private final static int ITEM_GAP = 10;
-	private final static int BLOCKSIZE = 48;
-	private int currentItem = -1;
 	private final float buttonSize;
-	private Rectangle bounds;
+	private final MiniMap minimap;
+	private final GuiItem itemSlots[];
+	private final float borderSize;
 
-	// top, right, bottom, left
-	private final static int OUTER_GAP[] = { 10, 5, 10, 10 };
-
-	/**
-	 * Width of the total inventory.
-	 */
-	public final static int WIDTH = BLOCKSIZE * 3 + 4 * ITEM_GAP;
-
-	private GuiItem itemSlots[];
-	private final float rectStroke = 3f;
+	private int currentItem = -1;
 
 	/**
 	 * Creates a new item toolbar.
@@ -74,95 +66,24 @@ public class ItemDisplay extends ClickableGuiElement implements
 	 */
 	public ItemDisplay(UserInterface gui, MiniMap map) {
 		super(gui);
+		this.minimap = map;
 		buttonSize = BLOCKSIZE * GameRenderer.getRenderScale();
-		screenX = map.getSize() + 5;
+		borderSize = 2 * GameRenderer.getRenderScale();
+		screenX = 15;
 		itemSlots = new GuiItem[Inventory.MAX_CAPACITY];
 		for (int i = 0; i < Inventory.MAX_CAPACITY; i++) {
 			itemSlots[i] = new GuiItem(gui, i);
 		}
-		registerAsTooltipTriggerer(this);
-		bounds = new Rectangle(screenX, screenY, buttonSize
-				* Inventory.MAX_CAPACITY / 2 + OUTER_GAP[1] + OUTER_GAP[3]
-				+ ITEM_GAP * (Inventory.MAX_CAPACITY / 2 - 1), buttonSize * 2);
 		setActiveInteractModes(InteractMode.MODE_EGO);
 	}
 
 	@Override
 	public void render(Graphics g) {
-		Font font = FontCache.getFont(FontKey.DEFAULT_FONT, g);
 		currentItem = getInfo().getClientData().getCurrentItemSelected();
-		g.setLineWidth(rectStroke);
-		for (GuiItem item : itemSlots) {
-
-			if (item.isEmpty()) {
-				continue;
-			}
-			Color currentColor = (item.getSlotId() == currentItem) ? Color.white
-					: COLOR_MULTIPLIER;
-			String idString = "#" + (item.getSlotId() + 1);
-			font.drawString(item.getX() + screenX + 1, item.getY() + screenY
-					- font.getLineHeight(), idString, currentColor);
-
-			if (item.isWeapon()) {
-				int ammo = item.getWeaponAmmu();
-				String ammoString = "*" + ammo;
-				font.drawString(
-						item.getX() + screenX + buttonSize
-								- font.getWidth(ammoString) - 3, item.getY()
-								+ screenY - font.getLineHeight(), ammoString,
-						currentColor);
-			}
-			g.setColor(currentColor);
-			Rectangle itemRect = item.getClickableRect();
-			if (item.hasImage()) {
-				g.drawImage(item.getItemImage(), itemRect.getX(),
-						itemRect.getY(), currentColor);
-			}
-			item.renderCooldown(g, itemRect.getX(), itemRect.getY(),
-					itemRect.getWidth(), itemRect.getHeight());
-			if (item.getSlotId() == currentItem) {
-				g.setColor(Color.yellow.multiply(COLOR_MULTIPLIER));
-			} else {
-				g.setColor(COLOR_MULTIPLIER);
-			}
-			g.draw(itemRect);
-		}
 	}
 
 	@Override
 	public void onGuiSizeChanged(int newWidth, int newHeight) {
-	}
-
-	@Override
-	public boolean mouseReleased(int button, int x, int y) {
-		for (int i = 0; i < Inventory.MAX_CAPACITY; i++) {
-			if (!itemSlots[i].isEmpty()
-					&& itemSlots[i].getClickableRect().contains(x, y)) {
-				itemClicked(i);
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
-	 * Indicates that an item has been clicked.
-	 * 
-	 * @param itemSlot
-	 *            item clicked.
-	 */
-	public void itemClicked(int itemSlot) {
-
-		try {
-			getInfo().getPlayer().getInventory().getItemBySlot(itemSlot);
-			getMouseHandler().itemClicked(itemSlot);
-			currentItem = itemSlot;
-
-		} catch (ItemSlotIsEmptyException e) {
-			L.fine("Player tried to use empty item slot " + itemSlot + ".");
-		} catch (ObjectNotFoundException e) {
-			L.log(Level.SEVERE, "Object not found", e);
-		}
 	}
 
 	@Override
@@ -171,6 +92,20 @@ public class ItemDisplay extends ClickableGuiElement implements
 			Item item = (Item) getInfo().getGameObjects().get(
 					event.getObjectId());
 			onItemChanged(event.getItemSlot(), item);
+		}
+		recalculatePosition();
+	}
+
+	private void recalculatePosition() {
+		try {
+			int items = getInfo().getPlayer().getInventory().getNumItems();
+			float height = (buttonSize + ITEM_GAP) * items - ITEM_GAP;
+			screenY = (Display.getHeight() - height - minimap.getSize()) / 2;
+			for (int i = 0; i < itemSlots.length; i++) {
+				itemSlots[i].updateClickRect();
+			}
+		} catch (ObjectNotFoundException e) {
+			L.log(Level.WARNING, "Player not found in itemdisplay.", e);
 		}
 	}
 
@@ -195,7 +130,6 @@ public class ItemDisplay extends ClickableGuiElement implements
 				L.log(Level.SEVERE,
 						"Missing cache image for: " + newItem.getType(), e);
 			}
-
 			itemSlots[slot].setName(newName);
 		}
 		itemSlots[slot].setItem(newItem);
@@ -208,7 +142,7 @@ public class ItemDisplay extends ClickableGuiElement implements
 	 * 
 	 */
 	private class GuiItem extends CooldownGuiObject {
-		private int x, y, slotId;
+		private int slotId;
 		private String name;
 		private Item item;
 		private Image itemImage;
@@ -216,20 +150,15 @@ public class ItemDisplay extends ClickableGuiElement implements
 
 		GuiItem(UserInterface gui, int slotId) {
 			super(gui);
-
 			this.slotId = slotId;
 			setName(EMPTY_NAME);
-			updateClickRect(0);
+			updateClickRect();
 		}
 
-		void updateClickRect(int lineHeight) {
-			this.x = OUTER_GAP[3] + ((int) buttonSize + ITEM_GAP)
-					* (slotId % (Inventory.MAX_CAPACITY / 2));
-			this.y = lineHeight + OUTER_GAP[0]
-					+ ((int) buttonSize + lineHeight + ITEM_GAP)
-					* (slotId / (Inventory.MAX_CAPACITY / 2));
-			clickRect = new Rectangle(x + getScreenX(), y + getScreenY(),
-					buttonSize, buttonSize);
+		void updateClickRect() {
+			clickRect = new Rectangle(getScreenX(), getScreenY()
+					+ Math.round((buttonSize + ITEM_GAP) * slotId), buttonSize,
+					buttonSize);
 		}
 
 		public void setItem(Item newItem) {
@@ -238,10 +167,6 @@ public class ItemDisplay extends ClickableGuiElement implements
 
 		public boolean hasImage() {
 			return itemImage != null;
-		}
-
-		Image getItemImage() {
-			return itemImage;
 		}
 
 		void setItemImage(Image image) {
@@ -253,7 +178,7 @@ public class ItemDisplay extends ClickableGuiElement implements
 		}
 
 		public boolean isEmpty() {
-			return name.equals(EMPTY_NAME) && getItemImage() == null;
+			return name.equals(EMPTY_NAME) && itemImage == null;
 		}
 
 		public int getWeaponAmmu() {
@@ -270,28 +195,47 @@ public class ItemDisplay extends ClickableGuiElement implements
 			return 0;
 		}
 
-		int getX() {
-			return x;
-		}
-
-		int getY() {
-			return y;
-		}
-
-		int getSlotId() {
-			return slotId;
-		}
-
 		void setName(String name) {
 			this.name = name;
 		}
 
-		protected Rectangle getClickableRect() {
-			return clickRect;
-		}
-
 		@Override
 		public void render(Graphics g) {
+			if (isEmpty()) {
+				return;
+			}
+			g.setLineWidth(1f);
+			Font font = FontCache.getFont(FontKey.TOOLTIP_FONT, g);
+			Color currentColor = (slotId == currentItem) ? Color.white
+					: COLOR_MULTIPLIER;
+
+			g.setColor(currentColor);
+
+			if (hasImage()) {
+				g.drawImage(itemImage, clickRect.getX(), clickRect.getY(),
+						currentColor);
+			}
+
+			if (slotId == currentItem) {
+				g.setColor(Color.yellow.multiply(COLOR_MULTIPLIER));
+			} else {
+				g.setColor(COLOR_MULTIPLIER);
+			}
+			if (isWeapon()) {
+				int ammo = getWeaponAmmu();
+				if (ammo != -1) {
+					String ammoString = ammo + "";
+					int stringWidth = font.getWidth(ammoString) + FONT_PADDING;
+					float stringX = clickRect.getX() + borderSize
+							+ FONT_PADDING;
+					float stringY = clickRect.getY() + borderSize;
+					g.setColor(FONT_BACKGROUND);
+
+					font.drawString(stringX, stringY, ammoString, currentColor);
+				}
+			}
+			renderCooldown(g, clickRect.getX(), clickRect.getY(),
+					clickRect.getWidth(), clickRect.getHeight());
 		}
 
 		@Override
@@ -315,54 +259,7 @@ public class ItemDisplay extends ClickableGuiElement implements
 	}
 
 	@Override
-	public void onMouseOver(Vector2f p) {
-		for (int i = 0; i < Inventory.MAX_CAPACITY; i++) {
-			if (!itemSlots[i].isEmpty()
-					&& itemSlots[i].getClickableRect().contains(p.x, p.y)) {
-				try {
-					getTooltipHandler().showItemTooltip(
-							p,
-							getInfo().getPlayer().getInventory()
-									.getItemBySlot(i));
-					return;
-				} catch (ItemSlotIsEmptyException e) {
-
-				} catch (ObjectNotFoundException e) {
-
-				}
-			}
-		}
-		getTooltipHandler().hideTooltip();
-	}
-
-	@Override
-	public Rectangle getBounds() {
-		return getTriggerArea();
-	}
-
-	@Override
-	public Rectangle getTriggerArea() {
-		return bounds;
-	}
-
-	@Override
 	public void onGameReady() {
-		try {
-			int lineHeight = FontCache.getFont(FontKey.DEFAULT_FONT)
-					.getLineHeight();
-			bounds.setHeight(buttonSize * 2 + lineHeight * 2 + OUTER_GAP[0]
-					+ OUTER_GAP[2] + ITEM_GAP);
-			bounds.setWidth(buttonSize * Inventory.MAX_CAPACITY / 2
-					+ OUTER_GAP[1] + OUTER_GAP[3] + ITEM_GAP
-					* (Inventory.MAX_CAPACITY / 2 - 1));
-			screenY = Display.getHeight() - bounds.getHeight();
-			bounds.setY(screenY);
-			for (int i = 0; i < itemSlots.length; i++) {
-				itemSlots[i].updateClickRect(lineHeight);
-			}
-		} catch (CacheException e) {
-			System.out.println("no font");
-		}
 		Inventory playerInventory;
 		try {
 			playerInventory = getInfo().getPlayer().getInventory();
@@ -379,5 +276,6 @@ public class ItemDisplay extends ClickableGuiElement implements
 				// that's okay
 			}
 		}
+		recalculatePosition();
 	}
 }
